2a41c2e9b04e827a7dbbe58df7180d265febac03
Marcin Nowak <marcin.j.nowak@gmail.com>
2023-09-08 04:13:40 +0200

    add setup.py

---
+++ setup.py
@@ -0,0 +1,37 @@
+import os
+
+from setuptools import find_packages, setup
+
+README_PATH = os.path.join(os.path.dirname(__file__), "README.md")
+
+setup(
+    name="django-easysettings",
+    version="0.1",
+    description="Custom settings management for Django",
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Environment :: Web Environment",
+        "Framework :: Django",
+        "Framework :: Django :: 3.1",
+        "Framework :: Django :: 3.2",
+        "Framework :: Django :: 4.0",
+        "License :: OSI Approved :: BSD License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Topic :: Internet :: WWW/HTTP",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Intended Audience :: Developers",
+    ],
+    author="Marcin Nowak",
+    author_email="marcin.j.nowak@gmail.com",
+    url="https://gitlab.com/marcinjn/django-easysettings",
+    keywords="web python django config settings",
+    packages=find_packages("."),
+    include_package_data=True,
+    zip_safe=True,
+    # long_description=open(README_PATH).read(),
+    # long_description_content_type="text/markdown",
+)
